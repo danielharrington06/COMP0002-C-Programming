a5f06569c9a4f78acbad6571472640aea50e0322
Daniel <danielharrington06@outlook.com>
2025-10-01 14:53:19 +0100

    count.c

---
+++ count/count.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int counter = 0;
+    while (counter < 10)
+    {
+        printf("%i\n", counter);
+        counter = counter + 1;
+    }
+    return 0;
+}
