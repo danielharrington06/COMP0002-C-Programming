e969d68b9f0c08d657cb78e395cb8c06ff6245da
Daniel <danielharrington06@outlook.com>
2025-10-01 17:21:11 +0100

    q7

--- assignment-1/questions/q7.c
+++ assignment-1/questions/q7.c
@@ -4,5 +4,11 @@
 
 int main(void)
 {
-    
+    drawRect(230, 100, 40, 200); // main tower section
+    // bumpy bits
+    drawOval(220, 80, 60, 20);
+    drawOval(220, 60, 60, 20);
+    drawOval(220, 40, 60, 20);
+    drawRect(245, 20, 10, 20);
+    return 0;
 }
