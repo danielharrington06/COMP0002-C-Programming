c7613b1c6dd6d820517bf29f2141678fcd1f3fd3
Daniel <danielharrington06@outlook.com>
2025-10-17 17:20:13 +0100

    q1

---
+++ assignment-3/q1.c
@@ -0,0 +1,34 @@
+// Written by Daniel Harrington
+// This program takes the lengths of sides of a triangle and outputs the area and perimeter
+
+#include <stdio.h>
+#include <math.h>
+
+double CalculatePerimeter(double x, double y, double z) {
+    return x + y + z;
+}
+
+double CalculateArea(double x, double y, double z) {
+    double s = 0.5*CalculatePerimeter(x, y, z);
+    return sqrt(s*(s-x)*(s-y)*(s-z));
+}
+
+int main(void)
+{
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    printf("Please enter the sides of the triangle.\n");
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+
+    double perimeter = CalculatePerimeter(a, b, c);
+    double area = CalculateArea(a, b, c);
+
+    printf("The Perimeter is %lf\n", perimeter);
+    printf("The Area is %lf\n", area);
+
+}
