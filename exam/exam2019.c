8944eb6032c91fd07f38352f5166b84a0792f882
Daniel Harrington <danielharrington06@outlook.com>
2026-05-06 17:49:19 +0100

    part e

--- exam/exam2019.c
+++ exam/exam2019.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int countChar(char* string, char c) {
     int i = 0;
@@ -23,14 +24,60 @@ int* countVowels(char* string) {
     return counts;
 }
 
+typedef struct Node {
+    char* value;
+    struct Node* next;
+} Node;
+
+Node* newNode(char* string) {
+    Node* new = malloc(sizeof(Node));
+    char* stringCopy = calloc(sizeof(char), strlen(string)+1);
+    strcpy(stringCopy, string);
+    new->value = stringCopy;
+    new->next = NULL;
+    return new;
+}
+
+Node* convertStringArrayToLL(char* strings[], int size) {
+    Node* head = newNode(strings[0]);
+    if (head == NULL) {
+        printf("allocation failed for LL");
+        return NULL;
+    }
+    Node* prev = head;
+    for (int i = 1; i < size; i++) {
+        Node* curr = newNode(strings[i]);
+        if (curr == NULL) {
+            printf("allocation failed for LL");
+            // free it
+            return NULL;
+        }
+        prev->next = curr;
+        prev = curr;
+    }
+    return head;
+}
+
+void deleteLinkedList(Node* start) {
+    Node* curr = start;
+    while (curr != NULL) {
+        Node* next = curr->next;
+        free(curr->value);
+        free(curr);
+        curr = next;
+    }
+    return;
+}
+
 int main(int argc, char const *argv[])
 {
-    char *s = malloc(100);
-    printf("Please enter a string: ");
-    scanf(" %99[^\n]", s);
-    int* vowels = countVowels(s);
-    printf("Vowel distribution:\n");
-    printf("a: %d \t e: %d \t i: %d \t o: %d \t u: %d \n", vowels[0], vowels[1], vowels[2], vowels[3], vowels[4]);
-
+    char* strings[] = {"hi", "how are you", "whats up", "how you doing"};
+    Node* head = convertStringArrayToLL(strings, 4);
+    Node* curr = head;
+    for (int i = 0; i < 4; i++) {
+        printf("%s\n", curr->value);
+        curr = curr->next;
+    }
+    deleteLinkedList(head);
     return 0;
 }
